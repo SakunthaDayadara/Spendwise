bf5e4932f16741f1a956f8ebf4bcc186aec03dd7
Sakuntha <sakunthadayadara2001@gmail.com>
2023-10-24 18:52:16 +0530

    Icon and Welcome Screen

--- app/src/main/java/com/groupproject/spendwise/MainActivity_Welcome.kt
+++ app/src/main/java/com/groupproject/spendwise/MainActivity_Welcome.kt
@@ -3,9 +3,9 @@ package com.groupproject.spendwise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity_Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_welcome)
     }
 }
--- app/src/main/java/com/groupproject/spendwise/SplashScreen.kt
+++ app/src/main/java/com/groupproject/spendwise/SplashScreen.kt
@@ -3,9 +3,7 @@ package com.groupproject.spendwise
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler;
 
-import androidx.core.os.HandlerCompat.postDelayed
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
@@ -18,7 +16,7 @@ class SplashScreen : AppCompatActivity() {
 
         splash_logo.alpha = 0f
         splash_logo.animate().setDuration(2000).alpha(1f).withEndAction {
-            val i = Intent (this, MainActivity::class.java)
+            val i = Intent (this, MainActivity_Welcome::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
