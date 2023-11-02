cedf020ef08265d37aad22db1459e76b9199c2ae
Sakuntha <sakunthadayadara2001@gmail.com>
2023-11-03 00:40:21 +0530

    Navigations Done

--- app/src/main/java/com/groupproject/spendwise/Expenses.kt
+++ app/src/main/java/com/groupproject/spendwise/Expenses.kt
@@ -1,51 +1,44 @@
 package com.groupproject.spendwise
 
+
 import android.content.Intent
-import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.groupproject.spendwise.databinding.ActivityHomeBinding
-import com.groupproject.spendwise.databinding.ActivityLoginBinding
+
+import com.groupproject.spendwise.databinding.ActivityExpensesBinding
 import kotlinx.android.synthetic.main.activity_income.view.*
 
-class Home : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+class Expenses : AppCompatActivity() {
 
+    private lateinit var binding: ActivityExpensesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityExpensesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavigationView.setSelectedItemId(R.id.bottom_home)
+
+        binding.bottomNavigationView.setSelectedItemId(R.id.bottom_expense)
         binding.bottomNavigationView.setOnItemSelectedListener {
             if (it.itemId == R.id.bottom_home){
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
                 true
+
             }else if (it.itemId == R.id.bottom_income){
-                true
                 val intent = Intent(this, Income::class.java)
                 startActivity(intent)
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-
+                true
             }else if (it.itemId == R.id.bottom_expense){
                 true
-                val intent = Intent(this, Expenses::class.java)
-                startActivity(intent)
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-
             }else if (it.itemId == R.id.bottom_profile){
-                true
                 val intent = Intent(this, Profile::class.java)
                 startActivity(intent)
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-
+                true
             }
 
             true
